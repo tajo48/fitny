f348a0fa9da2e43d1b39854a439544e182770723
tajo48 <tajo48@proton.me>
2026-09-15 19:30:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    requests are cached as they happen. All paths are relative, so the worker
    works at any URL depth. */
 
-const VERSION = "2";
+const VERSION = "3";
 const CORE = "fitny-core-" + VERSION;
 const FONTS = "fitny-fonts-" + VERSION;
 
