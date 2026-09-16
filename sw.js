8d3e101b7877c21be9dddd4631eeca0a80b75fa2
tajo48 <tajo48@proton.me>
2026-09-16 20:25:15 +0200

    Add validation for onboarding profile fields

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    requests are cached as they happen. All paths are relative, so the worker
    works at any URL depth. */
 
-const VERSION = "12";
+const VERSION = "13";
 const CORE = "fitny-core-" + VERSION;
 const FONTS = "fitny-fonts-" + VERSION;
 
