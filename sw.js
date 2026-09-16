9b3f3bbff380e05072c16d5f1f2e6ddf00dbdab9
tajo48 <tajo48@proton.me>
2026-09-16 20:35:55 +0200

    Add automatic kcal goal adjustment based on intake and weight trend

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    requests are cached as they happen. All paths are relative, so the worker
    works at any URL depth. */
 
-const VERSION = "13";
+const VERSION = "14";
 const CORE = "fitny-core-" + VERSION;
 const FONTS = "fitny-fonts-" + VERSION;
 
